a6ff1e0035b0f5273f35961c28a15dd983d5a0a7
Timo Westland <timo.westland@incentro.com>
2016-12-22 10:45:47 +0100

    Add base app structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './recipe-book/components/header/header.component';
+import { RecipesComponent } from './recipe-book/components/recipes/recipes.component';
+import { RecipeListComponent } from './recipe-book/components/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './recipe-book/components/recipe-list/recipe-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecipesComponent,
+    RecipeListComponent,
+    RecipeItemComponent
   ],
   imports: [
     BrowserModule,
